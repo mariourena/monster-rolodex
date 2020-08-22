940d78c021a0d2cec3335ff40d78ff0b8cb85821
Mario Urena <=>
2020-08-22 13:57:28 -0700

    Updates

---
+++ precache-manifest.85a7535408a5f34d0f4e05843deb1f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0decf17f58ab92423431eb772f268cae",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "37ec3b6b536f5d36c995",
+    "url": "/monster-rolodex/static/css/main.8df6f90c.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ec3b6b536f5d36c995",
+    "url": "/monster-rolodex/static/js/main.82c99f96.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
